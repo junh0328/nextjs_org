c128e89ae434d418d5c8277a06fc19a3a9c47434
이준희 <junh0328@naver.com>
2021-02-15 20:30:34 +0900

    next에 대한 주석 추가

--- nextjs-blog/lib/posts.js
+++ nextjs-blog/lib/posts.js
@@ -9,6 +9,7 @@ export function getSortedPostsData() {
   const fileNames = fs.readdirSync(postsDirectory);
   const allPostsData = fileNames.map((fileName) => {
     // Remove ".md" from file name to get id
+    // [ pre-rendering.md > pre-render, ssg-ssr.md > ssg-ssr ]
     const id = fileName.replace(/\.md$/, '');
 
     // Read markdown file as string
--- nextjs-blog/pages/posts/[id].js
+++ nextjs-blog/pages/posts/[id].js
@@ -1,6 +1,23 @@
 import Layout from '../../components/layout';
 import { getAllPostIds, getPostData } from '../../lib/posts';
 
+export async function getStaticProps({ params }) {
+  const postData = getPostData(params.id);
+  return {
+    props: {
+      postData,
+    },
+  };
+}
+
+export async function getStaticPaths() {
+  const paths = getAllPostIds();
+  return {
+    paths, // getAllPostIds() 함수에 의해 리턴되는 path 배열을 가지고 있습니다. [{ id : pre-rendering },{ id: ssg-ssr } ]
+    fallback: false, // fallback 속성은 나중에 설명합니다.
+  };
+}
+
 export default function Post({ postData }) {
   console.log(postData);
   return (
@@ -12,20 +29,4 @@ export default function Post({ postData }) {
       {postData.date}
     </Layout>
   );
-}
-export async function getStaticPaths() {
-  const paths = getAllPostIds();
-  return {
-    paths, // getAllPostIds() 함수에 의해 리턴되는 path 배열을 가지고 있습니다. [{ id : pre-rendering },{ id: ssg-ssr } ]
-    fallback: false, // fallback 속성은 나중에 설명합니다.
-  };
-}
-
-export async function getStaticProps({ params }) {
-  const postData = getPostData(params.id);
-  return {
-    props: {
-      postData,
-    },
-  };
 }
